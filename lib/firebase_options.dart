6efde6f8572f96807c570e5c7faf5c9ccebbca59
sindele <sindele@gmail.com>
2025-11-23 15:04:17 +0000

    toll jetzt wieder alles von hand

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -22,25 +22,25 @@ class DefaultFirebaseOptions {
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for android - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          '''DefaultFirebaseOptions have not been configured for android - 
+          you can reconfigure this by running the FlutterFire CLI again.''',
         );
       case TargetPlatform.iOS:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - 
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          '''DefaultFirebaseOptions have not been configured for ios - 
+          you can reconfigure this by running the FlutterFire CLI again.''',
         );
       case TargetPlatform.macOS:
         return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          '''DefaultFirebaseOptions have not been configured for windows - 
+          you can reconfigure this by running the FlutterFire CLI again.''',
         );
       case TargetPlatform.linux:
         throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+          '''DefaultFirebaseOptions have not been configured for linux - 
+          you can reconfigure this by running the FlutterFire CLI again.''',
         );
       default:
         throw UnsupportedError(
